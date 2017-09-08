22df400da2f75101e99e6f0749f03bf9435be286
Michalina Pacholska <micha7a@gmail.com>
2017-09-08 14:47:00 +0200

    Moved _array functions to printing_patterns
    They make more sense there, as they are not reused anywhere, they just
    produce an array.

--- printing_tools.py
+++ printing_tools.py
@@ -83,8 +83,8 @@ def move(p0, p_curr, s_max, ds, coord):
 
     return p, array
 
-def wait(p0, p_curr, points):
 
+def wait(p0, p_curr, points):
     return p_curr, [list(p0 + p_curr)]*points
 
 
@@ -175,113 +175,9 @@ def pattern2array3d_pair(rate, pattern, speed, z_steps, delta_z,
                             cut=True, margin=margin))
 
 
-def height_test(rate, line_len, z_steps,
-                speed_limit=10.0, p0=np.array([0.0, 0.0, 100.0]), z_range=100.0, x_range=100.0, margin=5.0):
-    """This is a specific pattern to print in order to check if the height 
-    is correct"""
-
-    p = [0, 0, 0]
-    array = [np.copy(p0)]
-    ds = speed_limit * rate
-    delta_z = z_range / z_steps
-    line_dist_x = (x_range - line_len) / z_steps
-
-    for z in range(z_steps):
-        y0 = p[1]
-        x0 = p[0]
-        p, tmp_arr = move_slow(p0, p, y0 + line_len, ds, 1, margin)
-        array += list(tmp_arr)
-        p, tmp_arr = wait(p0, p, 2)
-        array += list(tmp_arr)
-        p, tmp_arr = move_slow(p0, p, y0 + line_len * z / z_steps, ds, 1, margin)
-        array += list(tmp_arr)
-        p[1] = y0 + line_len * z / z_steps
-        p, tmp_arr = move_slow(p0, p, x0 + line_len, ds, 0, margin)
-        array += list(tmp_arr)
-        p, tmp_arr = wait(p0, p, 2)
-        array += list(tmp_arr)
-        p, tmp_arr = move_slow(p0, p, x0, ds, 0, margin)
-        array += list(tmp_arr)
-        p[0] = x0
-        p, tmp_arr = move_slow(p0, p, y0, ds, 1, margin)
-        array += list(tmp_arr)
-        p[1] = y0
-        if z < z_steps - 1:
-            p, tmp_arr = wait(p0, p, 2)
-            array += list(tmp_arr)
-            p, tmp_arr = move(p0, p, x0 + line_dist_x, ds, 0)
-            array += list(tmp_arr)
-            p[0] = x0 + line_dist_x
-            p, tmp_arr = move(p0, p, -(z + 1) * delta_z, ds, 2)
-            array += list(tmp_arr)
-            p[2] = -(z + 1) * delta_z
-    return array
-
-
 def intensity2speed(t, dot_size, intensity, max_dots):
     intensity /= np.max(intensity)
     return dot_size / (intensity * t * max_dots)
-
-
-def square_wave_array(p0, dx, nx, stepsx, dy, ny):
-    """"Simple square wave in x and y directions
-    which whe didn't managed to print """""
-    assert (nx * ny < 3333)
-    assert (dx * nx * stepsx < 100.0e3)
-    assert (dy * ny < 100.0e3)
-    array = []
-    p = list(p0)
-    for idx in range(nx):
-        for _ in range(stepsx):
-            p[0] += dx
-            array.append(list(p))
-        if idx % 2 == 0:
-            for y in range(ny):
-                p[1] += dy
-                array.append(list(p))
-        else:
-            for y in range(ny):
-                p[1] -= dy
-                array.append((list(p)))
-    return array
-
-
-def circle_array(radius, n, p0):
-    """"Simple circle, or rather regular polygon with n vertices"""""
-    array = []
-    for phi in np.linspace(0, 2 * np.pi, n):
-        p = list(p0)
-        p[0] += np.sin(phi) * radius
-        p[1] += (1 + np.cos(phi)) * radius
-        array.append(list(p))
-    return array
-
-
-def cross_array(max_shift, z_range=100, x_range=100, y_range=100,
-                p0=np.array([50, 50, 0])):
-    array = [np.copy(p0)]
-    p = np.array([0, 0, 0])
-    for z in np.arange(0, z_range, max_shift):
-        for x in np.arange(0, 0.5 * x_range, max_shift):
-            array.append(p0 + p)
-            p[0] = x
-        for x in np.arange(0.5 * x_range, -0.5 * x_range, -max_shift):
-            array.append(p0 + p)
-            p[0] = x
-        for x in np.arange(-0.5 * x_range, 0, max_shift):
-            array.append(p0 + p)
-            p[0] = x
-        for y in np.arange(0, 0.5 * y_range, max_shift):
-            array.append(p0 + p)
-            p[1] = y
-        for y in np.arange(0.5 * y_range, -0.5 * y_range, -max_shift):
-            array.append(p0 + p)
-            p[1] = y
-        for y in np.arange(-0.5 * y_range, 0, max_shift):
-            array.append(p0 + p)
-            p[1] = y
-        p[2] = z
-    return array
 
 
 def array2file(time_period, pattern, filename,
